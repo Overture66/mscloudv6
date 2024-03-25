17a5060b5c04b431f20b212d7fd7c01b2b0c52d3
Overture <chaoccc2022@163.com>
2024-03-25 19:30:38 +0800

    增加common-api模块,重构项目

--- cloud-consumer-order80/src/main/java/com/sc/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/sc/cloud/controller/OrderController.java
@@ -1,7 +1,7 @@
-package com.sc.controller;
+package com.sc.cloud.controller;
 
-import com.sc.entities.PayDTO;
-import com.sc.resp.ResultData;
+import com.sc.cloud.entities.PayDTO;
+import com.sc.cloud.resp.ResultData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
