5a72a6c96f3b2fb5092ff4de823e60293bedf68f
Overture <chaoccc2022@163.com>
2024-03-25 21:16:17 +0800

    安装consul(192.168.31.128:8500),完成服务注册与发现

--- cloud-provider-payment8001/src/main/java/com/sc/Main8001.java
+++ cloud-provider-payment8001/src/main/java/com/sc/Main8001.java
@@ -2,10 +2,12 @@ package com.sc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.sc.cloud.mapper")
+@EnableDiscoveryClient
 public class Main8001 {
     public static void main(String[] args) {
         SpringApplication.run(Main8001.class, args);
