1c54d778076ecbae068fbb0ccc5b0fc3a76487f4
Overture <chaoccc2022@163.com>
2024-03-25 19:05:46 +0800

    payment8001模块时间格式,统一返回值优化,解决全局返回异常

--- cloud-provider-payment8001/src/main/java/com/sc/cloud/controller/PayController.java
+++ cloud-provider-payment8001/src/main/java/com/sc/cloud/controller/PayController.java
@@ -2,12 +2,14 @@ package com.sc.cloud.controller;
 
 import com.sc.cloud.entities.Pay;
 import com.sc.cloud.entities.PayDTO;
+import com.sc.cloud.resp.ResultData;
 import com.sc.cloud.service.PayService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
+import com.sc.cloud.resp.ReturnCodeEnum;
 
 @RestController
 @Tag(name = "支付微服务模块",description = "支付CRUD")
@@ -16,29 +18,55 @@ public class PayController{
     PayService payService;
     @PostMapping(value = "/pay/add")
     @Operation(summary = "新增",description = "新增支付流水方法,json串做参数")
-    public String addPay(@RequestBody Pay pay){
+    public ResultData<String> addPay(@RequestBody Pay pay)
+    {
         System.out.println(pay.toString());
         int i = payService.add(pay);
-        return "成功插入记录，返回值："+i;
+        return ResultData.success("成功插入记录，返回值："+i);
     }
+
     @DeleteMapping(value = "/pay/del/{id}")
     @Operation(summary = "删除",description = "删除支付流水方法")
-    public Integer deletePay(@PathVariable("id") Integer id) {
-        return payService.delete(id);
+    public ResultData<Integer> deletePay(@PathVariable("id") Integer id) {
+        int i = payService.delete(id);
+        return ResultData.success(i);
     }
+
     @PutMapping(value = "/pay/update")
     @Operation(summary = "修改",description = "修改支付流水方法")
-    public String updatePay(@RequestBody PayDTO payDTO){
+    public ResultData<String> updatePay(@RequestBody PayDTO payDTO)
+    {
         Pay pay = new Pay();
         BeanUtils.copyProperties(payDTO, pay);
 
         int i = payService.update(pay);
-        return "成功修改记录，返回值："+i;
+        return ResultData.success("成功修改记录，返回值："+i);
     }
+
     @GetMapping(value = "/pay/get/{id}")
     @Operation(summary = "按照ID查流水",description = "查询支付流水方法")
-    public Pay getById(@PathVariable("id") Integer id){
-        return payService.getById(id);
+    public ResultData<Pay> getById(@PathVariable("id") Integer id)
+    {
+        if(id == -4) throw new RuntimeException("id不能为负数");
+
+        Pay pay = payService.getById(id);
+        return ResultData.success(pay);
     }
+
     //TODO 全部查询getall作为家庭作业
+
+    @RequestMapping(value = "/pay/error",method = RequestMethod.GET)
+    public ResultData<Integer> getPayError()
+    {
+        Integer i = Integer.valueOf(200);
+        try
+        {
+            System.out.println("--------come here");
+            int data = 10/0;
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResultData.fail(ReturnCodeEnum.RC500.getCode(),e.getMessage());
+        }
+        return ResultData.success(i);
+    }
 }
