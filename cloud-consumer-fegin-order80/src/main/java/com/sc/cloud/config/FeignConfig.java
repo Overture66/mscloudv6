ea24435ac3f70b9e2a063df01d14e0ec1cd5deea
Overture <chaoccc2022@163.com>
2024-03-26 19:43:48 +0800

    OpenFeign开启日志打印功能

--- cloud-consumer-fegin-order80/src/main/java/com/sc/cloud/config/FeignConfig.java
+++ cloud-consumer-fegin-order80/src/main/java/com/sc/cloud/config/FeignConfig.java
@@ -1,5 +1,6 @@
 package com.sc.cloud.config;
 
+import feign.Logger;
 import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,5 +15,9 @@ public class FeignConfig
 
         //最大请求次数为3(1+2)，初始间隔时间为100ms，重试间最大间隔时间为1s
         return new Retryer.Default(100,1,3);
+    }
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 }
