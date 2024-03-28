78bb4aae34462f3eb1f89a4fbf29dc52edabf65b
Overture <chaoccc2022@163.com>
2024-03-28 16:54:44 +0800

    gateway完成基础配置,端口映射

---
+++ cloud-provider-payment8001/src/main/java/com/sc/cloud/controller/PayGateWayController.java
@@ -0,0 +1,34 @@
+package com.sc.cloud.controller;
+
+import cn.hutool.core.util.IdUtil;
+import com.sc.cloud.entities.Pay;
+import com.sc.cloud.resp.ResultData;
+import com.sc.cloud.service.PayService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+
+@RestController
+public class PayGateWayController
+{
+    @Resource
+    PayService payService;
+
+    @GetMapping(value = "/pay/gateway/get/{id}")
+    public ResultData<Pay> getById(@PathVariable("id") Integer id)
+    {
+        Pay pay = payService.getById(id);
+        return ResultData.success(pay);
+    }
+
+    @GetMapping(value = "/pay/gateway/info")
+    public ResultData<String> getGatewayInfo()
+    {
+        return ResultData.success("gateway info test："+ IdUtil.simpleUUID());
+    }
+}
