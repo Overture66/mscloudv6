e4eae0b52062384cf8d41a1a8f4dba30afb174e6
Overture <chaoccc2022@163.com>
2024-03-29 10:03:55 +0800

    增加AddRequestHeader GatewayFilter Factory

--- cloud-provider-payment8001/src/main/java/com/sc/cloud/controller/PayGateWayController.java
+++ cloud-provider-payment8001/src/main/java/com/sc/cloud/controller/PayGateWayController.java
@@ -1,19 +1,24 @@
 package com.sc.cloud.controller;
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.IdUtil;
 import com.sc.cloud.entities.Pay;
 import com.sc.cloud.resp.ResultData;
 import com.sc.cloud.service.PayService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Enumeration;
 import java.util.concurrent.TimeUnit;
 
 
 @RestController
+@Slf4j
 public class PayGateWayController
 {
     @Resource
@@ -30,5 +35,23 @@ public class PayGateWayController
     public ResultData<String> getGatewayInfo()
     {
         return ResultData.success("gateway info test："+ IdUtil.simpleUUID());
+    }
+
+    @GetMapping(value = "/pay/gateway/filter")
+    public ResultData<String> getGatewayFilter(HttpServletRequest request)
+    {
+        String result = "";
+        Enumeration<String> headers = request.getHeaderNames();
+        while(headers.hasMoreElements())
+        {
+            String headName = headers.nextElement();
+            String headValue = request.getHeader(headName);
+            log.info("请求头名: " + headName +"\t\t\t"+"请求头值: " + headValue);
+            if(headName.equalsIgnoreCase("X-Request-sc1")
+                    || headName.equalsIgnoreCase("X-Request-sc2")) {
+                result = result+headName + "\t " + headValue +" ";
+            }
+        }
+        return ResultData.success("getGatewayFilter 过滤器 test： "+result+" \t "+ DateUtil.now());
     }
 }
