ecb0303ec46bc6896dc5d61ac6d6cbeeaaf7c7de
Overture <chaoccc2022@163.com>
2024-04-01 16:27:09 +0800

    Sentinel-热点规则

--- cloudalibaba-sentinel-service8401/src/main/java/com/sc/cloud/controller/RateLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/sc/cloud/controller/RateLimitController.java
@@ -5,6 +5,7 @@ import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -47,6 +48,18 @@ public class RateLimitController
     public String doActionFallback(@PathVariable("p1") Integer p1,Throwable e){
         log.error("程序逻辑异常了:{}", e);
         return "程序逻辑异常了"+"\t"+e.getMessage();
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "dealHandler_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+
+                             @RequestParam(value = "p2",required = false) String p2){
+        return "------testHotKey";
+    }
+    public String dealHandler_testHotKey(String p1,String p2,BlockException exception)
+    {
+        return "-----dealHandler_testHotKey";
     }
 
 }
