3208dcd93dbaf1881ab83890b3a57ee75852d3e2
Overture <chaoccc2022@163.com>
2024-04-01 11:51:00 +0800

    Sentinel-流控模式-直接,预热,排队等待(代码)

--- cloudalibaba-sentinel-service8401/src/main/java/com/sc/cloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/sc/cloud/controller/FlowLimitController.java
@@ -37,5 +37,12 @@ public class FlowLimitController
         flowLimitService.common();
         return "------testD";
     }
+
+    @GetMapping("/testE")
+    public String testE()
+    {
+        System.out.println(System.currentTimeMillis()+"      testE,排队等待");
+        return "------testE";
+    }
 }
  
